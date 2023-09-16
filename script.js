90e3395a81d70a42c4422a84f13a4d0be37e832b
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-16 13:55:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+function changestring() {
+  var element = document.getElementById("status");
+
+  // Create a new element with the desired tag name
+  var newElement = document.createElement("h1");
+
+  // Copy the content from the old element to the new one
+  newElement.innerHTML = element.innerHTML;
+  // Set the new inner text for the element
+  newElement.innerText = "Entered the Metaverse";
+  // Replace the old element with the new one
+  element.parentNode.replaceChild(newElement, element);
+}
