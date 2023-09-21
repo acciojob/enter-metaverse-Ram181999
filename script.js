5e12ec1fad8b911aabc1f43cab52ecb460ccff72
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-21 20:01:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 //your JS code here. If required.
-
+const para =document.getElementById("status");
 function changestring() {
-  var element = document.getElementById("status");
 
-  // Create a new element with the desired tag name
-  var newElement = document.createElement("h1");
-
-  // Copy the content from the old element to the new one
-  newElement.innerHTML = element.innerHTML;
-  // Set the new inner text for the element
-  newElement.innerText = "Entered the Metaverse";
-  // Replace the old element with the new one
-  element.parentNode.replaceChild(newElement, element);
+	para.innerHTML="<h1>Entered the Metaverse</h1>" ;
+	
 }
